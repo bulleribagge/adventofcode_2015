b5ee21613733b63adc575687fbc9a81048317b0a
bulleribagge <get_get_martin@hotmail.com>
2015-12-30 15:24:04 +0100

    second part of day 1 done

--- day1/app.js
+++ day1/app.js
@@ -7,5 +7,19 @@ fs.readFile('input.txt', 'utf8', function(error, data){
 	fdata = data;
 	ups = fdata.match(/\(/g).length;
 	downs = fdata.match(/\)/g).length;
+	var floor = 0;
+	var position = 1;
+	console.log(fdata.length);
+	for (var c of fdata) 
+	{
+		floor += c == "(" ? 1 : -1;
+		//console.log(floor);
+		if(floor == -1)
+		{
+			console.log("basement reached on position: " + position);
+			break;
+		}
+		position++;
+	};
 	console.log("floor: " + parseInt(ups - downs));
 });
