99eeb62b3542eeb64de3844e68b8e2aa6480988c
bulleribagge <get_get_martin@hotmail.com>
2016-01-09 12:42:49 +0100

    first part of day10 done

---
+++ day10/app.js
@@ -0,0 +1,43 @@
+var input = "3113322113";
+var curr = input;
+
+for(var i = 0; i < 40; i++)
+{
+	//console.log(curr);
+	curr = lookAndSay(curr);
+	//console.log(curr);
+}
+
+console.log(curr.length);
+
+function lookAndSay(str)
+{
+	var res = "";
+	var last = str[0];
+	var groups = [];
+	var groupCount = 0;
+
+	for(var i = 0; i < str.length; i++)
+	{
+		if(groups.length > 0)
+		{
+			if(str[i] == groups[groupCount][0])
+			{
+				groups[groupCount] += str[i];
+			}else
+			{
+				groups[++groupCount] = str[i];
+			}
+		}else
+		{
+			groups[groupCount] = str[i];
+		}
+	}
+
+	for(var g of groups)
+	{
+		res += g.length + g[0];
+	}
+
+	return res;
+}
